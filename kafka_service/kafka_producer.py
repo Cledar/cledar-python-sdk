b5581f2791283b77ae1b242e1382220946aba663
Piotr Komorowski <piotr.komorowski@cledar.com>
2024-07-03 12:55:59 +0000

    KSR-218 "Implement periodic connection check"

--- kafka_service/kafka_producer.py
+++ kafka_service/kafka_producer.py
@@ -1,3 +1,5 @@
+import threading
+import time
 from confluent_kafka import Producer, KafkaException
 from settings import Settings
 from .utils import build_topic, delivery_callback
@@ -7,9 +9,11 @@ from .exceptions import KafkaConnectionError, KafkaProducerNotConnectedError
 
 class KafkaProducer:
     def __init__(self, config: Settings) -> None:
-        self.config = config
+        self.config: Settings = config
         logger.info("Initializing KafkaProducer.", extra={"config": config})
-        self.producer = None
+        self.producer: Producer | None = None
+        self.connection_check_thread: threading.Thread | None = None
+        self._stop_event: threading.Event = threading.Event()
 
     def connect(self) -> None:
         self.producer = Producer(
@@ -23,29 +27,57 @@ class KafkaProducer:
             "Connected to Kafka servers.",
             extra={"kafka_servers": self.config.kafka_servers},
         )
+        self.start_connection_check_thread()
 
-    def send(self, topic: str, key: str, value: str) -> None:
+    def start_connection_check_thread(self) -> None:
+        if self.connection_check_thread is None:
+            self.connection_check_thread = threading.Thread(
+                target=self._monitor_connection
+            )
+            self.connection_check_thread.start()
+            logger.info(
+                "Started kafka connection check thread.",
+                extra={"interval": self.config.kafka_connection_check_interval_sec},
+            )
+
+    def _monitor_connection(self) -> None:
+        while not self._stop_event.is_set():
+            try:
+                self.check_connection()
+                logger.info("Kafka connection status: Connected.")
+            except KafkaConnectionError as exception:
+                logger.error(
+                    "Kafka connection check failed.",
+                    extra={"exception": str(exception)},
+                )
+            time.sleep(self.config.kafka_connection_check_interval_sec)
+
+    def send(self, topic: str, value: str, key: str) -> None:
         if self.producer is None:
             logger.error(
                 "KafkaProducer is not connected. Call 'connect' first.",
-                extra={"topic": topic, "key": key, "value": value},
+                extra={"topic": topic, "value": value, "key": key},
             )
             raise KafkaProducerNotConnectedError
 
         topic = build_topic(topic_name=topic, prefix=self.config.kafka_topic_prefix)
 
         try:
-            logger.info(
+            logger.debug(
                 "Sending message to topic.",
-                extra={"topic": topic, "key": key, "value": value},
+                extra={"topic": topic, "value": value, "key": key},
             )
-            self.producer.produce(topic, key, value, callback=delivery_callback)
+            self.producer.produce(
+                topic=topic, value=value, key=key, callback=delivery_callback
+            )
             self.producer.poll(0)
 
         except BufferError:
             logger.warning("Buffer full, waiting for free space on the queue")
-            self.producer.poll(self.config.kafka_block_buffer_time)
-            self.producer.produce(topic, key, value, callback=delivery_callback)
+            self.producer.poll(self.config.kafka_block_buffer_time_sec)
+            self.producer.produce(
+                topic=topic, value=value, key=key, callback=delivery_callback
+            )
 
         except KafkaException as exception:
             logger.error(
@@ -62,7 +94,9 @@ class KafkaProducer:
         the below is far-from-perfect workaround handling that
         """
         try:
-            self.producer.list_topics(timeout=1)
+            self.producer.list_topics(
+                timeout=self.config.kafka_connection_check_timeout_sec
+            )
         except KafkaException as exception:
             logger.error(
                 "Failed to connect to Kafka servers.",
@@ -70,7 +104,10 @@ class KafkaProducer:
             )
             raise KafkaConnectionError from exception
 
-    def prepare_for_shutdown(self) -> None:
+    def shutdown(self) -> None:
+        self._stop_event.set()
+        if self.connection_check_thread is not None:
+            self.connection_check_thread.join()  # Wait for the thread to finish
         logger.info("Flushing KafkaProducer.")
         self.producer.flush(-1)
         logger.info("KafkaProducer flushed.")
