57a3a138e518dfaa39133993158ab1e27120db86
Michal Gora <aroglahcim@gmail.com>
2024-09-11 15:15:48 +0200

    fix(KSR-402): rename to S3ItemMetadata

--- s3_service/url.py
+++ s3_service/url.py
@@ -2,7 +2,7 @@ import dataclasses
 
 
 @dataclasses.dataclass
-class S3Metadata:
+class S3ItemMetadata:
     bucket: str
     key: str
 
@@ -11,14 +11,14 @@ class IncorrectSchemaException(Exception):
     """Url needs to start with `s3://`"""
 
 
-def parse_url(url: str) -> S3Metadata:
+def parse_url(url: str) -> S3ItemMetadata:
     if not url.startswith("s3://"):
         raise IncorrectSchemaException
 
     just_data = url.replace("s3://", "")
     bucket, key = just_data.split("/", maxsplit=1)
 
-    return S3Metadata(
+    return S3ItemMetadata(
         bucket=bucket,
         key=key,
     )
