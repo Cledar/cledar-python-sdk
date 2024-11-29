1ed5f6298f1656c1512480396086b51edf306267
Kajetan Husiatyński <kajetan.husiatynski@cledar.com>
2024-11-29 08:42:12 +0000

    feat(KSR-549): In Kafka rename 'inputs' to 'data'

--- kafka_service/tests/test_input_parser.py
+++ kafka_service/tests/test_input_parser.py
@@ -1,5 +1,5 @@
 # pylint: disable=too-many-instance-attributes
-from typing import Any
+from typing import Any, List, TypeVar, Generic
 from collections.abc import Sequence
 import pytest
 from faker import Faker
@@ -96,67 +96,83 @@ class PipelineMessageReference:
     created_by: str
 
 
-@pydantic.dataclasses.dataclass
-class PipelineStageReference(PipelineMessageReference):
-    topic_name: str
-    inputs: Sequence[dict[str, Any] | PipelineMessageReference]
+StreamInfo = AudioStreamInfo | VideoStreamInfo | SubtitleStreamInfo
+PipelineMessageData = TypeVar("PipelineMessageData", bound=pydantic.BaseModel)
 
 
 @pydantic.dataclasses.dataclass
-class PipelineMessageMetadata:
+class PipelineMessageDataContainer(Generic[PipelineMessageData]):
+    data: dict[str, Any] | PipelineMessageData
+
+
+@pydantic.dataclasses.dataclass
+class PipelineStageReference(
+    Generic[PipelineMessageData],
+    PipelineMessageReference,
+    PipelineMessageDataContainer[PipelineMessageData],
+):
+    output_topic_name: str
+    parent_ids: List[str]
+
+
+@pydantic.dataclasses.dataclass
+class PipelineMessageMetadata(Generic[PipelineMessageData]):
     origin: ContentOrigin
-    streams: list[VideoStreamInfo | AudioStreamInfo | SubtitleStreamInfo]
-    pipeline_stages: list[PipelineStageReference]
+    streams: List[StreamInfo]
+    pipeline_stages: List[PipelineStageReference[PipelineMessageData]]
 
 
 @pydantic.dataclasses.dataclass
-class PipelineMessage(PipelineMessageReference):
-    metadata: PipelineMessageMetadata
+class PipelineMessage(
+    Generic[PipelineMessageData],
+    PipelineMessageReference,
+    PipelineMessageDataContainer[PipelineMessageData],
+):
+    metadata: PipelineMessageMetadata[PipelineMessageData]
 
 
-class InputMessagePayload(pydantic.BaseModel, PipelineMessage):
-    id: str
+class InputMessageData(pydantic.BaseModel):
     chunks: Sequence[S3Metadata]
 
 
-@pydantic.dataclasses.dataclass
-class InputMessageReference(PipelineMessageReference):
-    id: str
-    chunks: Sequence[S3Metadata]
+class InputMessagePayload(pydantic.BaseModel, PipelineMessage[InputMessageData]):
+    data: InputMessageData
 
 
 fake = Faker()
 
 # pylint: disable=line-too-long
-input_json_value = '{"id":"78c7392659984c2d89057bd628275855","created_at":"2024-08-30T11:09:56Z","created_by":"chunk-transformer","metadata":{"origin":{"id":"dbd5f857572e49bfb8dd678dd14a4b85","name":"TVP_1","url":"https://example.com","mode":"broadcast","target":"television","network":{"id":null,"name":"korbank"},"distributor":{"id":null,"name":"TVP_1"},"metadata":{"id":null,"medium":"video","type":"channel"}},"streams":[{"id":"2cad60eac39f4cb4a676eb001350c3c3","type":"video","index":0,"codec_name":"h264","format":"yuv420p","width":1920,"height":1080,"display_aspect_ratio":"16:9","pixel_aspect_ratio":"1:1","pixel_format":"yuv420p","framerate":"25:1","delay":0},{"id":"9a8c7216c1e24f9eb2152c52ac3feb50","type":"audio","index":1,"codec_name":"mp2","language":"pol","format":"s16p","channels":2,"channels_layout":"stereo","sample_rate":48000,"bit_rate":192000,"frame_size":1152,"delay":0},{"id":"50bc1f29f4b84b28a92aa263567dd9ca","type":"audio","index":2,"codec_name":"ac3","language":"qaa","format":"fltp","channels":6,"channels_layout":"5.1(side)","sample_rate":48000,"bit_rate":384000,"frame_size":0,"delay":0},{"id":"eb2f0753258b4d0fbf77af82ac7cff59","type":"subtitle","index":4,"codec_name":"dvbsub","language":"pol","delay":0},{"id":"dc57b3f8fdb04420bc820fe1d45985b6","type":"audio","index":6,"codec_name":"mp2","language":"aux","format":"s16p","channels":2,"channels_layout":"stereo","sample_rate":48000,"bit_rate":128000,"frame_size":1152,"delay":0}],"pipeline_stages":[{"id":"c577dd6d2be743d5a155ef75d6d1ec79","created_at":"2024-08-30T11:09:55Z","created_by":"stream-chunker","topic_name":"reference-chunks","inputs":[]},{"id":"78c7392659984c2d89057bd628275855","created_at":"2024-08-30T11:09:56Z","created_by":"chunk-transformer","topic_name":"reference-transformed-chunks","inputs":[{"id":"f11a8990bfff4288b9b284cb9908c788","created_at":"2024-08-30T11:09:55Z","created_by":"stream-chunker","s3_key":"0285ee22c95f43f9a98792d210aa6813.mpegts","s3_bucket":"reference-chunks","chunk_duration":5,"chunk_overlap_sec":0,"chunk_size":5179024,"ignored_streams":[3,5,7]}]}]},"chunks":[{"url":"s3://reference-transformed-chunks/video/0285ee22c95f43f9a98792d210aa6813_stream_0.mp4","type":"video","container":"mpegts"},{"url":"s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_1.mp4","type":"audio","container":"mpegts"},{"url":"s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_2.mp4","type":"audio","container":"mpegts"},{"url":"s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_6.mp4","type":"audio","container":"mpegts"}]}'
+input_json_value = '{"data":{"chunks":[{"url":"s3://reference-transformed-chunks/video/0285ee22c95f43f9a98792d210aa6813_stream_0.mp4","type":"video","container":"mpegts"},{"url":"s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_1.mp4","type":"audio","container":"mpegts"},{"url":"s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_2.mp4","type":"audio","container":"mpegts"},{"url":"s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_6.mp4","type":"audio","container":"mpegts"}]},"id":"78c7392659984c2d89057bd628275855","created_at":"2024-08-30T11:09:56Z","created_by":"chunk-transformer","metadata":{"origin":{"id":"dbd5f857572e49bfb8dd678dd14a4b85","name":"TVP_1","url":"https://example.com","mode":"broadcast","target":"television","network":{"id":null,"name":"korbank"},"distributor":{"id":null,"name":"TVP_1"},"metadata":{"id":null,"medium":"video","type":"channel"}},"streams":[{"id":"2cad60eac39f4cb4a676eb001350c3c3","type":"video","index":0,"codec_name":"h264","format":"yuv420p","width":1920,"height":1080,"display_aspect_ratio":"16:9","pixel_aspect_ratio":"1:1","pixel_format":"yuv420p","framerate":"25:1","delay":0},{"id":"9a8c7216c1e24f9eb2152c52ac3feb50","type":"audio","index":1,"codec_name":"mp2","language":"pol","format":"s16p","channels":2,"channels_layout":"stereo","sample_rate":48000,"bit_rate":192000,"frame_size":1152,"delay":0},{"id":"50bc1f29f4b84b28a92aa263567dd9ca","type":"audio","index":2,"codec_name":"ac3","language":"qaa","format":"fltp","channels":6,"channels_layout":"5.1(side)","sample_rate":48000,"bit_rate":384000,"frame_size":0,"delay":0},{"id":"eb2f0753258b4d0fbf77af82ac7cff59","type":"subtitle","index":4,"codec_name":"dvbsub","language":"pol","delay":0},{"id":"dc57b3f8fdb04420bc820fe1d45985b6","type":"audio","index":6,"codec_name":"mp2","language":"aux","format":"s16p","channels":2,"channels_layout":"stereo","sample_rate":48000,"bit_rate":128000,"frame_size":1152,"delay":0}],"pipeline_stages":[{"data":{},"id":"c577dd6d2be743d5a155ef75d6d1ec79","created_at":"2024-08-30T11:09:55Z","created_by":"stream-chunker","output_topic_name":"reference-chunks","parent_ids":[]},{"data":{"id":"f11a8990bfff4288b9b284cb9908c788","created_at":"2024-08-30T11:09:55Z","created_by":"stream-chunker","s3_key":"0285ee22c95f43f9a98792d210aa6813.mpegts","s3_bucket":"reference-chunks","chunk_duration":5,"chunk_overlap_sec":0,"chunk_size":5179024,"ignored_streams":[3,5,7]},"id":"78c7392659984c2d89057bd628275855","created_at":"2024-08-30T11:09:56Z","created_by":"chunk-transformer","output_topic_name":"reference-transformed-chunks","parent_ids":[]}]}}'
 
 # pylint: disable=unexpected-keyword-arg
 expected = InputMessagePayload(  # type: ignore
     id="78c7392659984c2d89057bd628275855",
     created_at="2024-08-30T11:09:56Z",
     created_by="chunk-transformer",
-    chunks=[
-        S3Metadata(
-            url="s3://reference-transformed-chunks/video/0285ee22c95f43f9a98792d210aa6813_stream_0.mp4",
-            type="video",
-            container="mpegts",
-        ),
-        S3Metadata(
-            url="s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_1.mp4",
-            type="audio",
-            container="mpegts",
-        ),
-        S3Metadata(
-            url="s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_2.mp4",
-            type="audio",
-            container="mpegts",
-        ),
-        S3Metadata(
-            url="s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_6.mp4",
-            type="audio",
-            container="mpegts",
-        ),
-    ],
+    data=InputMessageData(
+        chunks=[
+            S3Metadata(
+                url="s3://reference-transformed-chunks/video/0285ee22c95f43f9a98792d210aa6813_stream_0.mp4",
+                type="video",
+                container="mpegts",
+            ),
+            S3Metadata(
+                url="s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_1.mp4",
+                type="audio",
+                container="mpegts",
+            ),
+            S3Metadata(
+                url="s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_2.mp4",
+                type="audio",
+                container="mpegts",
+            ),
+            S3Metadata(
+                url="s3://reference-transformed-chunks/audio/0285ee22c95f43f9a98792d210aa6813_stream_6.mp4",
+                type="audio",
+                container="mpegts",
+            ),
+        ],
+    ),
     metadata=PipelineMessageMetadata(
         origin=ContentOrigin(
             id="dbd5f857572e49bfb8dd678dd14a4b85",
@@ -239,27 +255,27 @@ expected = InputMessagePayload(  # type: ignore
                 id="c577dd6d2be743d5a155ef75d6d1ec79",
                 created_at="2024-08-30T11:09:55Z",
                 created_by="stream-chunker",
-                topic_name="reference-chunks",
-                inputs=[],
+                output_topic_name="reference-chunks",
+                parent_ids=[],
+                data={},
             ),
             PipelineStageReference(
                 id="78c7392659984c2d89057bd628275855",
                 created_at="2024-08-30T11:09:56Z",
                 created_by="chunk-transformer",
-                topic_name="reference-transformed-chunks",
-                inputs=[
-                    {
-                        "id": "f11a8990bfff4288b9b284cb9908c788",
-                        "created_at": "2024-08-30T11:09:55Z",
-                        "created_by": "stream-chunker",
-                        "s3_key": "0285ee22c95f43f9a98792d210aa6813.mpegts",
-                        "s3_bucket": "reference-chunks",
-                        "chunk_duration": 5,
-                        "chunk_overlap_sec": 0,
-                        "chunk_size": 5179024,
-                        "ignored_streams": [3, 5, 7],
-                    }
-                ],
+                parent_ids=[],
+                output_topic_name="reference-transformed-chunks",
+                data={
+                    "id": "f11a8990bfff4288b9b284cb9908c788",
+                    "created_at": "2024-08-30T11:09:55Z",
+                    "created_by": "stream-chunker",
+                    "s3_key": "0285ee22c95f43f9a98792d210aa6813.mpegts",
+                    "s3_bucket": "reference-chunks",
+                    "chunk_duration": 5,
+                    "chunk_overlap_sec": 0,
+                    "chunk_size": 5179024,
+                    "ignored_streams": [3, 5, 7],
+                },
             ),
         ],
     ),
@@ -270,7 +286,8 @@ def test_parse_1() -> None:
     parser = InputParser(InputMessagePayload)
     obj = parser.parse_json(input_json_value)
 
-    assert obj.chunks == expected.chunks
+    assert obj.data == expected.data
+    assert obj.data.chunks == expected.data.chunks
     assert obj.id == expected.id
     assert obj.created_at == expected.created_at
     assert obj.created_by == expected.created_by
