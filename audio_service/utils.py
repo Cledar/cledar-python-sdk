ca5f92a09111af1164064fbbc1292692ea563662
Karol Pustelnik <karol.pustelnik@cledar.com>
2025-04-26 14:42:37 +0200

    fix(KSR-955): lint

--- audio_service/utils.py
+++ audio_service/utils.py
@@ -1,8 +1,9 @@
+from __future__ import annotations
 import tempfile
 from tempfile import _TemporaryFileWrapper
 
 
-def load_audio_as_file(file_format: str, media: bytes) -> _TemporaryFileWrapper:
+def load_audio_as_file(file_format: str, media: bytes) -> _TemporaryFileWrapper[bytes]:
     if file_format not in ["mp3", "wav", "flac"]:
         file_format = "wav"
 
